b723ee757e2ff94b7c6cd0a59133b015d7987c66
Ray Andrew <raydreww@gmail.com>
2020-06-16 16:46:22 +0700

    change sleep between pulse

--- libdevice/stepper.cpp
+++ libdevice/stepper.cpp
@@ -7,7 +7,7 @@
 NAMESPACE_BEGIN
 
 namespace device {
-const time_unit StepperDevice::step_high_min = 1;
+const time_unit StepperDevice::step_high_min = 3;
 
 StepperDevice::StepperDevice(PI_PIN        step_pin,
                              PI_PIN        dir_pin,
