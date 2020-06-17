cd1b6098429f1bf0b3decca7f76a6ec51a91c2e8
Ray Andrew <raydreww@gmail.com>
2020-06-17 20:17:54 +0700

    fix config typo not found

--- libmechanism/movement.cpp
+++ libmechanism/movement.cpp
@@ -462,7 +462,7 @@ void Movement::follow_tending_paths() {
 
   for (const auto& iter : Config::get()->tending_path()) {
     LOG_INFO("Move to x={}mm y={}mm", iter.first, iter.second);
-    move<movement::unit::mm>(iter.first, iter.second, Config::get()->z());
+    move<movement::unit::mm>(iter.first, iter.second, State::get()->z());
   }
 
   revert_motor_params();
