4e987a97fa4bbf566717cd7eddaaa8a22e6dfb91
Ray Andrew <raydreww@gmail.com>
2020-07-21 21:48:22 +0700

    added task listener for timeout

--- libmachine/task-listener.cpp
+++ libmachine/task-listener.cpp
@@ -3,6 +3,7 @@
 #include "task-listener.hpp"
 
 #include <libdevice/device.hpp>
+#include <libutil/util.hpp>
 
 NAMESPACE_BEGIN
 
@@ -44,15 +45,56 @@ void TaskListener::stop() {
 }
 
 void TaskListener::execute() {
+  massert(Config::get() != nullptr, "sanity");
   massert(State::get() != nullptr, "sanity");
   // massert(device::DigitalInputDeviceRegistry::get() != nullptr, "sanity");
   massert(tsm()->is_ready(), "sanity");
 
   auto* state = State::get();
-  // auto* digital_input_registry = device::DigitalInputDeviceRegistry::get();
+  auto* config = Config::get();
+
+  time_unit start = seconds();
+  time_unit end = seconds();
 
   while (running()) {
     while (running() && (!state->homing())) {
+    }
+
+    if (!running()) {
+      return;
+    }
+
+    if (state->homing()) {
+      start = seconds();
+      end = seconds();
+    }
+
+    if (!running()) {
+      return;
+    }
+
+    while (running() && state->homing() &&
+           ((end - start) < config->timeout())) {
+      end = seconds();
+    }
+
+    if (!running()) {
+      return;
+    }
+
+    if (state->homing()) {
+      LOG_ERROR("[FAULT] Timeout while doing task!");
+      if (state->spraying_running()) {
+        LOG_ERROR("[FAULT] Last task: spraying");
+      } else if (state->tending_running()) {
+        LOG_ERROR("[FAULT] Last task: tending");
+      } else if (state->cleaning_running()) {
+        LOG_ERROR("[FAULT] Last task: cleaning");
+      }
+      state->fault(true);
+      tsm()->fault();
+    } else {
+      LOG_INFO("Homing task take about {} seconds", end - start);
     }
   }
 }
